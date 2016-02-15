61ec57807db303d0f55075cb2f19a3af17846d19
Sherlock Yao <yaosherlock@gmail.com>
2016-02-15 14:30:21 +0800

    add documentation and sample plist file

--- SYArtisanFrame/Routing/SYWireframe.swift
+++ SYArtisanFrame/Routing/SYWireframe.swift
@@ -21,6 +21,25 @@ public typealias SYWireframeViewControllerNavigator = (fromViewController: UIVie
     - wireframe look up configuration map for X with Port and Gate(optional), result Y
     - wireframe initialize Y with relevant builder and params
     - wireframe use relevant navigator to do the presentation from X to Y
+ 
+ 
+ The Port and Gate are simply string combinations to identiy a navigation for a given view controller,
+ for example:
+ if view controller X has three navigation point to three different view controllers
+ you can define X-Next-A, X-Next-B, X-Next-C or X-List, X-Detail, X-Setting for them, the rules are up to you.
+ But for a convenience and ease of use, it will be good to follow some certain rules when you define them.
+ The X is the Code you give to the view controller for short, remember to assign the real class name for it in the .plist setting file (section Decodes), so that the wireframe can find the right code for current navigating view controller automatically.
+ 
+ 
+ There are two ways for wirefirm initialize a view controller:
+    1. by storyboard, you can set the storyboard file name and the view controller's id in the .plist file
+    2. by code, you can assign a builder name in .plist file, also register that builder to wirefirm by code
+ Further more, if you want to configure your new view controllers, please subclass wireframe and override  `configureViewController:fromViewController:withParams:`
+ 
+ 
+ You can pick any presentation effect as you want for each navigation, assing the navigator name in the .plist file and register the navigator to wirefirm for specific method, the libaray already set up a default navigator set for quick start.
+ You can also set a transition apparatus for more customized transition animations.
+ 
 */
 public class SYWireframe {
     public static let defaultWireframe = SYWireframe(plistFileName: defaultPropertyListFileName)
